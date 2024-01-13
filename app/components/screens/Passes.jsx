211acdfeb7c8d1e21e0baebaf1e4b281f1dcfa79
Mayada <mayadayacoub@outlook.com>
2024-01-13 18:50:41 +0200

    Remove pass date from ui and route.js in passes

--- app/components/screens/Passes.jsx
+++ app/components/screens/Passes.jsx
@@ -65,7 +65,7 @@ export default function Locations({
       // await response.json then execute the following
       let passes = await response.json();
    
-      console.log(passes,'passes');
+      // console.log(passes);
 
       const tempData = [];
 
@@ -73,9 +73,9 @@ export default function Locations({
         let parts = pass.image_name.split("_");
 
        
-        let passDate = moment(pass.Pass_Date, "YYYY-MM-DD HH:mm:ss").format(
-          "MM/DD/YYYY h:mm a"
-        );
+        // let passDate = moment(pass.Pass_Date, "YYYY-MM-DD HH:mm:ss").format(
+        //   "MM/DD/YYYY h:mm a"
+        // );
 
         let processedDate = moment(
           parts[parts.length - 1],
@@ -97,7 +97,7 @@ export default function Locations({
         pass.error_end_time = error_end_time;
 
         tempData.push({
-          passDate: passDate,
+          // passDate: passDate,
           processedDate: processedDate,
           ...pass,
         });
@@ -263,12 +263,12 @@ export default function Locations({
                         >
                           Pass ID
                         </th>
-                        <th
+                        {/* <th
                           scope="col"
                           className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                         >
                           Pass Date
-                        </th>
+                        </th> */}
                         <th
                           scope="col"
                           className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
@@ -322,7 +322,7 @@ export default function Locations({
                               ? "N/A"
                               : pass.Pass_ID }
                           </td>
-                          <td
+                          {/* <td
                             className={classNames(
                               idx !== displayData.length - 1
                                 ? "border-b border-gray-200"
@@ -333,7 +333,7 @@ export default function Locations({
                             {pass.passDate == "Invalid date"
                               ? "N/A"
                               : pass.passDate}
-                          </td>
+                          </td> */}
                           <td
                             className={classNames(
                               idx !== displayData.length - 1
