821315fc35723be1c8aae85f773d4942bc91ebf3
hanan1312 <nana.elmogey@gmail.com>
2024-02-06 19:13:13 +0200

    tst

--- app/components/screens/Passes.jsx
+++ app/components/screens/Passes.jsx
@@ -277,18 +277,18 @@ console.log(hasError,'test has error')
                         >
                           #
                         </th>
-                        <th
-                          scope="col"
-                          className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
-                        >
-                          Pass ID
-                        </th>
                         {/* <th
                           scope="col"
                           className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                         >
-                          Pass Date
+                          Pass ID
                         </th> */}
+                        <th
+                          scope="col"
+                          className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
+                        >
+                          Pass Date
+                        </th>
                         <th
                           scope="col"
                           className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
@@ -344,9 +344,9 @@ console.log(hasError,'test has error')
                               "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8"
                             )}
                           >
-                            {pass.Pass_ID == "Invalid date"
+                            {pass.Pass_Date == "Invalid date"
                               ? "N/A"
-                              : pass.Pass_ID }
+                              : pass.Pass_Date }
                           </td>
                           <td
                             className={classNames(
